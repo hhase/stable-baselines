881f9d5db2f35190663ee0e82b252151a4617e10
Hannes Hase <hannes.hase@tum.de>
2020-01-14 09:08:03 +0100

    Cluster run

--- stable_baselines/common/__init__.py
+++ stable_baselines/common/__init__.py
@@ -4,6 +4,6 @@ from stable_baselines.common.dataset import Dataset
 from stable_baselines.common.math_util import discount, discount_with_boundaries, explained_variance, \
     explained_variance_2d, flatten_arrays, unflatten_vector
 from stable_baselines.common.misc_util import zipsame, set_global_seeds, boolean_flag
-from stable_baselines.common.base_class import BaseRLModel, ActorCriticRLModel, OffPolicyRLModel, SetVerbosity, \
-    TensorboardWriter
+from stable_baselines.common.base_class import BaseRLModel, OffPolicyRLModel, SetVerbosity, \
+    TensorboardWriter#, ActorCriticRLModel #ToDo - remove comment here
 from stable_baselines.common.cmd_util import make_vec_env
