e6bb2125114cd7496f789ea2db4f3c0180dc7e78
Hannes Hase <hannes.hase@tum.de>
2020-01-16 16:26:43 +0100

    Cluster run fix

--- stable_baselines/common/policies.py
+++ stable_baselines/common/policies.py
@@ -21,11 +21,18 @@ def custom_cnn(scaled_images, **kwargs):
     activ = tf.nn.relu
     layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=8, stride=4, init_scale=np.sqrt(2), **kwargs))
     layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
-    layer_3 = activ(conv(layer_2, 'c3', n_filters=128, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
-    layer_4 = activ(conv(layer_3, 'c4', n_filters=256, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
-    layer_5 = activ(conv(layer_4, 'c5', n_filters=512, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
-    layer_5 = conv_to_fc(layer_5)
-    return activ(linear(layer_5, 'fc1', n_hidden=1028, init_scale=np.sqrt(2)))
+    layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = conv_to_fc(layer_3)
+    return activ(linear(layer_3, 'fc1', n_hidden=512, init_scale=np.sqrt(2)))
+
+    #activ = tf.nn.relu
+    #layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=8, stride=4, init_scale=np.sqrt(2), **kwargs))
+    #layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
+    #layer_3 = activ(conv(layer_2, 'c3', n_filters=128, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    #layer_4 = activ(conv(layer_3, 'c4', n_filters=256, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    #layer_5 = activ(conv(layer_4, 'c5', n_filters=512, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    #layer_5 = conv_to_fc(layer_5)
+    #return activ(linear(layer_5, 'fc1', n_hidden=1028, init_scale=np.sqrt(2)))
 
 
 def nature_cnn(scaled_images, **kwargs):
